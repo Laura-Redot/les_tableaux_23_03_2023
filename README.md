78499aaa9b5c1eeb6c6fd8ae1a35a502b1829b34
Laura-Redot <laura.redot@gmail.com>
2023-03-24 09:47:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# les_tableaux_23_03_2023
+ Exercices sur les tableaux du 23/03/2023
---
+++ ex_1/les_tableaux_ex_1.js
@@ -0,0 +1,3 @@
+var mesDonnees = new Array("Paul", "Valérie", "Pierre");
+mesDonnees.length;
+alert ("Le premier élément du tableau est " + mesDonnees[0] + ".");
